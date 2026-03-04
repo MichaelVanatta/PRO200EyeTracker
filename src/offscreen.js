edcc5b5a3df6f324e6040eea67c71df7d693ebae
Nate W <nwillett@student.neumont.edu>
2026-03-04 09:46:59 -0700

    one more change

--- src/offscreen.js
+++ src/offscreen.js
@@ -1,3 +1,14 @@
+const phraseData = [
+  { phrase: "click", boost: 5.0 },
+  { phrase: "select", boost: 5.0 },
+  { phrase: "type", boost: 5.0 },
+  { phrase: "write", boost: 5.0 },
+];
+
+const phrases = phraseData.map(
+  (p) => new webkitSpeechRecognitionPhrase(p.phrase, p.boost),
+);
+
 chrome.runtime.onMessage.addListener(async (msg) => {
   if (msg.type === "startSR") {
     const recognition = new webkitSpeechRecognition();
@@ -5,6 +16,8 @@ chrome.runtime.onMessage.addListener(async (msg) => {
     recognition.continuous = true;
     recognition.interimResults = true;
     recognition.maxAlternatives = 1;
+    recognition.processLocally = true;
+    recognition.phrases = phraseObjects;
 
     recognition.onstart = () => {
       console.log("it go");
